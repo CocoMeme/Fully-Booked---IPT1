a8fb9da7d4ca8a5a0a60ecba4e41805c90446798
CocoMeme <coandrei04@gmail.com>
2024-10-18 23:42:05 +0800

    Top Sellers

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import bannerImg from "../../assets/banner.png "
+import bannerImg from "../../assets/banner.png"
 
 const Banner = () => {
   return (
-    <div>
-        <div>
+    <div className='flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12'>
+        <div className='md:w-1/2 w-full flex items-center md:justify-end'>
+            <img src={bannerImg} alt="" />
+        </div>           
+        
+        <div className='md:w-1/2 w-full'>
             <h1 className='md:text-5xl text-2xl font-medium mb-7'>Welcom to Fully Booked!</h1>
             <p className='mb-10'>
                 Its time to update your reading list with some of the latest and greatest releases in the literary world. 
@@ -14,9 +18,7 @@ const Banner = () => {
                 Shop Now!
             </button>
         </div>
-        <div>
-            <img src={bannerImg} alt="" />
-        </div>   
+
     </div>
   )
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from './Banner.jsx'
+import TopSellers from './TopSellers.jsx'
+
 
 function Home() {
   return (
     <>
       <Banner/>
+      <TopSellers/>
     </>
   )
 }
